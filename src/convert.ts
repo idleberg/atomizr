6b04e3fcf22f1d04167470d2ffbebd46eb2a349a
Jan T. Sott <jan@idleberg.com>
2017-09-30 20:23:02 +0200

    add conversion methods

--- src/convert.ts
+++ src/convert.ts
@@ -112,6 +112,23 @@ const sublime2vscode = (input, options) => {
     return output;
 };
 
+const sublime2yasnippet = (input, options) => {
+    let data, output;
+
+    try {
+        if (options.is_snippet === true) {
+            data = SublimeText.readXml(input, options);
+        } else {
+            data = SublimeText.readJson(input, options);
+        }
+        output = Yasnippet.write(data);
+    } catch (error) {
+        throw error;
+    }
+
+    return output;
+};
+
 // TextMate to anything
 
 const textmate2atom = (input, options) => {
@@ -153,6 +170,19 @@ const textmate2vscode = (input, options) => {
     return output;
 };
 
+const textmate2yasnippet = (input, options) => {
+    let data, output;
+
+    try {
+        data = TextMate.read(input, options);
+        output = Yasnippet.write(data);
+    } catch (error) {
+        throw error;
+    }
+
+    return output;
+};
+
 // Visual Studio Code to anything
 
 const vscode2atom = (input, options) => {
@@ -187,6 +217,19 @@ const vscode2textmate = (input, options) => {
     try {
         data = Vscode.read(input, options);
         output = TextMate.write(data);
+    } catch (error) {
+        throw error;
+    }
+
+    return output;
+};
+
+const vscode2yasnippet = (input, options) => {
+    let data, output;
+
+    try {
+        data = Vscode.read(input, options);
+        output = Yasnippet.write(data);
     } catch (error) {
         throw error;
     }
@@ -238,6 +281,9 @@ const convert = (input, targetDir, opts) => {
             } else if (opts.target === 'vscode' || opts.target === 'code') {
                 output = this.sublime2vscode(inputFile, {is_snippet: isSnippet, scope: scope, ignore_separator: ignore_separator});
                 targetFile = `${baseName}.json`;
+            } else if (opts.target === 'yasnippet' || opts.target === 'yas') {
+                output = this.sublime2yasnippet(inputFile, {is_snippet: isSnippet, scope: scope, ignore_separator: ignore_separator});
+                targetFile = baseName;
             } else if (opts.target === 'sublime' || opts.target === 'subl') {
                 return console.log(`Skipping "${input}"`);
             } else {
@@ -253,6 +299,9 @@ const convert = (input, targetDir, opts) => {
             } else if (opts.target === 'vscode' || opts.target === 'code') {
                 output = this.textmate2vscode(inputFile, {scope: scope});
                 targetFile = `${baseName}.json`;
+            } else if (opts.target === 'yasnippet' || opts.target === 'yas') {
+                output = this.textmate2yasnippet(inputFile, {scope: scope});
+                targetFile = baseName;
             } else if (opts.target === 'textmate' || opts.target === 'mate') {
                 return console.log(`Skipping "${input}"`);
             } else {
@@ -268,6 +317,9 @@ const convert = (input, targetDir, opts) => {
             } else if (opts.target === 'textmate' || opts.target === 'mate') {
                 output = this.vscode2textmate(inputFile, {scope: scope});
                 targetFile = `${baseName}.tmSnippet`;
+            } else if (opts.target === 'yasnippet' || opts.target === 'yas') {
+                output = this.vscode2yasnippet(inputFile, {scope: scope});
+                targetFile = baseName;
             } else if (opts.target === 'vscode' || opts.target === 'code') {
                 return console.log(`Skipping "${input}"`);
             } else {
@@ -296,12 +348,15 @@ export {
     sublime2atom,
     sublime2textmate,
     sublime2vscode,
+    sublime2yasnippet,
     textmate2atom,
     textmate2sublime,
     textmate2vscode,
+    textmate2yasnippet,
     vscode2atom,
     vscode2sublime,
     vscode2textmate,
+    vscode2yasnippet,
     atom2yasnippet,
     convert
 };
